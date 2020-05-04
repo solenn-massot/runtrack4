c4ff907d92e2bc9de6ea9a1b978103d693975d25
[solenn] <solenn.massot@laplateforme.io>
2020-05-04 16:10:44 +0200

    //jour01 je me met sur mon site perso

---
+++ jour01/job04/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<nav role="navigation">
+  <div id="menuToggle">
+   
+    <input type="checkbox" />
+    
+    <span></span>
+    <span></span>
+    <span></span>
+    
+    <ul id="menu">
+      <a href="#"><li>Accueil</li></a>
+      <a href="#"><li>Présentation</li></a>
+      <a href="#"><li>Contact</li></a>
+    </ul>
+  </div>
+</nav>
+</body>
+
+</html>
