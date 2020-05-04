07c6f1f7d12fdb9ce93400b6688e0198a1d0bad7
[solenn] <solenn.massot@laplateforme.io>
2020-05-04 12:00:42 +0200

    //job 01, job02, job03 fait

---
+++ jour01/job01/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" media="screen and (min-width: 1680px) and (max-width: 1980px)" href="style2.css">
+    <link rel="stylesheet" media="screen and (min-width: 1280px) and (max-width: 1680px)" href="style3.css">
+    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="style4.css">
+</head>
+
+<body>
+</body>
+
+</html>
---
+++ jour01/job03/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section class="section">
+    
+    </section>
+    <section class="section">
+        
+    </section>
+    <section class="section">
+        
+    </section>
+    <section class="section">
+        
+    </section>
+</body>
+
+</html>
