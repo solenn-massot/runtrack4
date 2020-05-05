afc056ef446dc677ed81c48cb04aa6783052f9f0
[solenn] <solenn.massot@laplateforme.io>
2020-05-05 17:13:40 +0200

    //finish j'aurais bien aimé changer les typos et quelques détails mais c'est apparemment impossible sans refaire un fichier.css derrière

---
+++ jour02/job05/index.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</head>
+
+<body>
+    <header>
+        <nav class="blue-grey darken-2">
+        <div class="container">
+            <div class="nav-wrapper">
+                <a href="#!" class="brand-logo"><i class="material-icons">audiotrack</i>Diskaire</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="index.php">Votre liste d'envie</a></li>
+                    <li><a href="index.php">Panier</a></li>
+                </ul>
+            </div>
+            <div class="container">
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <section class="section">
+            <h2>Nos albums préférés</h2>
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img  class="responsive-img" src="img/Horizons.jpg">
+                            <span class="card-title">Horizons - Temnozor</span>
+                            <a class="btn-floating halfway-fab waves-effect waves- blue-grey darken-3"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>Un album où se mêlent les nuances du black le plus pur au folk énergique de la Russie profonde </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m6">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img class="responsive-img" src="img/iron.jpg">
+                            <span class="card-title">Iron - Ensiferum</span>
+                            <a class="btn-floating halfway-fab waves-effect waves- blue-grey darken-3"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>L'épique du folk dans toute sa splendeur, des chants viking et des batailles de dragon pour le plaisir de nos oreilles</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+        </div>
+        <div class="container">
+            <h4>Dernières sorties</h4>
+            <section>
+            <div class="row">
+                <div class="col s12 m12">
+                <div class="col s4 m4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img class="responsive-img" src="img/black.jpg">
+                            <span class="card-title">Black Album - Metallica</span>
+                            <a class="btn-floating halfway-fab waves-effect waves- blue-grey darken-3"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>L'album classique par excellence. Du Metallica old school, donc leur meilleur période.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s4 m4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img class="responsive-img" src="img/otta.jpg">
+                            <span class="card-title">Ótta - Sólstafir</span>
+                            <a class="btn-floating halfway-fab waves-effect waves- blue-grey darken-3"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>Ces islandais sont décidemment les meilleurs pour les musiques lentes et mélancoliques. A écouter par temps de pluie.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s4 m4">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img class="responsive-img" src="img/skalmold.jpg">
+                            <span class="card-title">Baldur - Skálmöld</span>
+                            <a class="btn-floating halfway-fab waves-effect waves- blue-grey darken-3"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>La crème de la crème du viking metal. Notez également que leur album avec l'orchestre symphonique d'Islande est tout aussi bon.</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+        </div>
+    </main>
+    <footer class="page-footer  blue-grey darken-3">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Ceci est un footer</h5>
+
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="index.php">Accueil</a></li>
+                        <li><a class="grey-text text-lighten-3" href="index.php">Votre liste d'envie</a></li>
+                        <li><a class="grey-text text-lighten-3" href="index.php">Panier</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright   blue-grey darken-4">
+            <div class="container">
+                © 2014 Copyright Text
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
